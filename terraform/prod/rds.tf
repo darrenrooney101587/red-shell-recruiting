52c504e5fead6372745560e21bef65757ffb93df
Darren Rooney <darren.rooney@benchmarkanalytics.com>
2025-04-20 15:19:30 -0400

    Added rds to terraform

---
+++ terraform/prod/rds.tf
@@ -0,0 +1,24 @@
+resource "aws_db_subnet_group" "rds_public" {
+  name       = "rds-public-subnet-group"
+  subnet_ids = var.rds_subnet_ids
+
+  tags = {
+    Name = "rds-public-subnet-group"
+  }
+}
+
+resource "aws_db_instance" "postgres" {
+  identifier              = var.db_identifier
+  engine                  = "postgres"
+  engine_version          = "15.3"
+  instance_class          = "db.t3.micro"
+  allocated_storage       = 20
+  name                    = "postgres"
+  username                = var.db_username
+  password                = var.db_password
+  publicly_accessible     = true
+  db_subnet_group_name    = aws_db_subnet_group.rds_public.name
+  vpc_security_group_ids  = [aws_security_group.red_shell_prod_sg.id]
+  skip_final_snapshot     = true
+  availability_zone       = "us-east-2b"
+}
---
+++ terraform/prod/terraform.tfvars
@@ -0,0 +1,4 @@
+ami_id            = "ami-02f761c2f5c9bbbe5"
+eip_allocation_id = "eipalloc-09592bf7d01c5e4ba"
+db_password     = "your-secure-db-password"
+rds_subnet_ids  = ["subnet-04b5e85714dc5b310", "subnet-0feeb254527036127"]
