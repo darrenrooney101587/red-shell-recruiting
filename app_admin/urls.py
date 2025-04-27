9822d718bba06ecdd293e46787b0547f89b971fe
Darren Rooney <darren.rooney@benchmarkanalytics.com>
2025-04-26 20:29:24 -0400

    Checkpoint for post

--- app_admin/urls.py
+++ app_admin/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from app_admin.views import privacy_policy, terms_of_service
 from red_shell_recruiting.views import index
 from red_shell_recruiting.views import healthcheck
 
@@ -11,4 +12,6 @@ urlpatterns = [
     path("mechanic/", include("mechanic.urls")),
     path("account/", include("account.urls")),
     path("candidate/", include("red_shell_recruiting.urls")),
+    path('privacy_policy/', privacy_policy, name='privacy-policy'),
+    path('terms_of_service/', terms_of_service, name='terms-of-service'),
 ]
