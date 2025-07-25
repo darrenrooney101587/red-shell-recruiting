95eb82218dd9b3a2c1096ae9f3f7c8356aaf2c08
Darren Rooney <darren.rooney@benchmarkanalytics.com>
2025-07-25 18:37:40 -0400

    Checkpoint

--- build/launch_local.sh
+++ build/launch_local.sh
@@ -1,2 +1,4 @@
-docker-compose -f docker-compose.local.yml  --env-file .env down
+docker-compose -f docker-compose.local.yml down --remove-orphans -v
+read -p "Run docker system prune to clean up old networks? [y/N]: " confirm && \
+[ "$confirm" = "y" ] && docker system prune --force
 docker-compose -f docker-compose.local.yml  --env-file .env up -d
