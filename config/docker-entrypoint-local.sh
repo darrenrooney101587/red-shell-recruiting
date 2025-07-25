089a7bce52a35d066a1354c65d6db4fb17548d2b
Darren Rooney <darren.rooney@benchmarkanalytics.com>
2025-07-25 19:22:03 -0400

    Checkpoint

---
+++ config/docker-entrypoint-local.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+poetry run python manage.py migrate || { echo 'Migration failed!'; }
+
+echo 'Collecting static files...(this may start the application by default)'
+poetry run python manage.py collectstatic --noinput --clear || { echo 'Failed to clear static files!'; }
+echo 'Done collecting static files....'
+
+chmod -R a+rX /app/staticfiles || { echo 'Failed to set static file permissions!'; }
+
+echo "Starting Gunicorn for local/dev..."
+exec poetry run gunicorn app_admin.wsgi \
+    --name admin \
+    --bind 0.0.0.0:5000 \
+    --timeout 300 \
+    --access-logfile - \
+    --error-logfile - \
+    --log-config config/gunicorn_logging.conf \
+    --log-level=info
