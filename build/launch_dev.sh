94b07cbea811d604edc9546de189abf7f3e5e757
Darren Rooney <darren.rooney@benchmarkanalytics.com>
2025-07-25 20:21:44 -0400

    Local -> dev

--- build/launch_dev.sh
+++ build/launch_dev.sh
@@ -1,4 +1,4 @@
 docker-compose -f docker-compose.dev.yml down --remove-orphans -v
 read -p "Run docker system prune to clean up old networks? [y/N]: " confirm && \
 [ "$confirm" = "y" ] && docker system prune --force
-docker-compose -f docker-compose.dev.yml --env-file .env up  --build -d
+docker-compose -f docker-compose.dev.yml --env-file .env.dev up  --build -d
